8252a0fa77a2e04697d1f9373af61c8b2cfafcd0
CSECURITY\ConvergenceSecurity <lch10017@gmail.com>
2017-01-23 13:17:44 +0900

    제출

--- routes/welcome.js
+++ routes/welcome.js
@@ -6,28 +6,28 @@ const http = require('http');
 
 /* GET home page. */
 // html파일 위치 root/login.html
-// router.get('/', function(req, res) {
-//   fs.readFile('welcome.ejs',(err,data)=>{
-//     if(err){
-//       console.log(err);
-//     }else{
-//       // res.send(req.session);
-//       // res.send(req.session.displayName);
-//       res.writeHead(200,{
-//         'Content-Type':'text/html'
-//       });
-//       res.end(data);
-//     }
-//   });
-// });
-
-router.get('/',function(req,res) {
-  fs.readFile('/welcome.ejs','utf8',function(err,data){
-    response.writeHead(200,{'Content-Type': 'text/html'});
-    response.end(ejs.render(data));
+router.get('/', function(req, res) {
+  fs.readFile('welcome.ejs',(err,data)=>{
+    if(err){
+      console.log(err);
+    }else{
+      // res.send(req.session);
+      // res.send(req.session.displayName);
+      res.writeHead(200,{
+        'Content-Type':'text/html'
+      });
+      res.end(data);
+    }
   });
-  // res.render('welcome', { name: 'req.session.displayName' });
 });
+
+// router.get('/',function(req,res) {
+//   fs.readFile('/welcome.ejs','utf8',function(err,data){
+//     response.writeHead(200,{'Content-Type': 'text/html'});
+//     response.end(ejs.render(data));
+//   });
+//   // res.render('welcome', { name: 'req.session.displayName' });
+// });
 
 
 
